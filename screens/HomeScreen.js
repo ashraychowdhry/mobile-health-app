f0f885bc32d24c056d16961cf103c22b6cb957e3
Ashray Chowdhry <ashraychowdhry@gmail.com>
2022-01-30 01:56:18 -0500

    working Polygon stock data pull-in

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,64 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { auth } from '../firebase';
+import axios from 'axios';
+import { KeyboardAvoidingView } from 'react-native';
 
-export default function HomeScreen () {
+export default function HomeScreen (props) {
 
     const [ticker, setTicker] = useState('');
+    const [marketOpen, setMarketOpen] = useState(null);
+
+
+    const getMarketOpen = async () => {
+        var query = 'https://api.polygon.io/v1/marketstatus/now?apiKey=EepXZYpz1RiHneHcnRHQupa8To6g53Dv'
+        
+        var data;
+
+        try {
+            data = await axios.get(query)
+            .then(response => {
+                alert('The market is ' + response.data.market);
+                if (response.data.market == 'closed') {
+                    setMarketOpen(false);
+                } else {
+                    setMarketOpen(true);
+                }
+            })
+        } catch (error) {
+            alert(error)
+        }
+        
+
+        /*
+        const data = axios.get(
+            query,
+            {
+            headers: {
+                'Authorization': 'Bearer EepXZYpz1RiHneHcnRHQupa8To6g53Dv'
+            }
+            }
+        );
+
+        */
+        
+        
+    }
+
+    useEffect(() => {
+
+        getMarketOpen();
+            
+    }, []);
 
     const nav = useNavigation();
     const handleViewData = () => {
 
+
+
+       
     }
     const handleLogOut = () => {
         auth
@@ -26,11 +74,15 @@ export default function HomeScreen () {
 
     
   return (
-    <View style={styles.container}>
-      <Text>Welcome, {auth.currentUser?.email}!</Text>
+    <KeyboardAvoidingView style={[styles.container, marketOpen? styles.openMarket : styles.closedMarket]} behavior='padding'>
+        <View style={styles.header}>
+            <Text style={[styles.headerText, marketOpen? styles.headerText : styles.darkModeText]}> Welcome, {auth.currentUser?.email}!</Text>
+            {marketOpen? <Text style={[styles.headerText, marketOpen? styles.headerText : styles.darkModeText]}> The market is open!</Text> : <Text style={[styles.headerText, marketOpen? styles.headerText : styles.darkModeText]}> The market is closed!</Text>}
+        </View>
+      
 
       <View style={styles.buttonContainer}>
-        <Text>Please enter a stock ticker you would like to view.</Text>
+        <Text style={[styles.text, marketOpen? styles.text : styles.darkModeText]}>Please enter a stock ticker you would like to view.</Text>
           <TextInput style={styles.input} placeholder='Ticker (Ex. AAPL)' />
           
             <TouchableOpacity style={styles.button} onPress={handleViewData} >
@@ -40,7 +92,7 @@ export default function HomeScreen () {
                 <Text style={styles.buttonOutlinedText}>Log Out</Text>
             </TouchableOpacity>
         </View>
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -50,7 +102,24 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    openMarket: {
+        backgroundColor: '#97ff94',
+    },
+    closedMarket: {
+        backgroundColor: '#1f1f1f',
+    },
+    headerText: {
+        fontSize: 25,
+        padding: 20,
+        marginTop: 40,
+        textAlign: 'center',
+    },
+    text: {
+        fontSize: 20,
+        padding: 30,
+        marginTop: 10,
+        textAlign: 'center',
     },
     inputsContainer: {
         width: '80%',
@@ -75,6 +144,7 @@ const styles = StyleSheet.create({
         width: '100%',
         padding: 15,
         borderRadius: 10,
+        alignItems: 'center',
     },
     buttonText: {
         color: 'white',
@@ -86,12 +156,16 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderColor: '#228CDB',
         borderWidth: 1,
-        marginTop: 5,
+        marginTop: 60,
     },
     buttonOutlinedText: {
         color: '#228CDB',
         fontSize: 18,
         fontWeight: '700',
+    },
+    darkModeText: {
+        color: 'white',
+
     },
     
 });
